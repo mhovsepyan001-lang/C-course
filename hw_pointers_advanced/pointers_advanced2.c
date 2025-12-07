d01feacf3bf0e48ac1a0d31c4fe30e9857bffc0f
Meri Hovsepyan <mhovsepyan001@gmail.com>
2025-12-07 15:14:31 +0400

    Fix scanf and printf pointer usage in matrix transpose

--- hw_pointers_advanced/pointers_advanced2.c
+++ hw_pointers_advanced/pointers_advanced2.c
@@ -11,15 +11,14 @@ int main() {
     printf("Enter the elements of the matrix:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            scanf("%d", *(matrix + i) + j); // pointer version of &matrix[i][j]
+            scanf("%d", *(matrix + i) + j); 
         }
     }
 
-    // Print transpose directly using pointers
     printf("Transpose of the matrix:\n");
     for (int j = 0; j < m; j++) {        
         for (int i = 0; i < n; i++) {
-            printf("%d ", *(*(matrix + i) + j)); // pointer version of matrix[i][j]
+            printf("%d ", *(*(matrix + i) + j)); 
         }
         printf("\n");
     }
