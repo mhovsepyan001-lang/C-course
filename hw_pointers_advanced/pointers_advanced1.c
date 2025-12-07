4e5469911fe5dfcbd32f21e64f95589f7ee45ed3
Meri Hovsepyan <mhovsepyan001@gmail.com>
2025-12-07 15:13:56 +0400

    Update pointers_advanced1.c

--- hw_pointers_advanced/pointers_advanced1.c
+++ hw_pointers_advanced/pointers_advanced1.c
@@ -2,30 +2,30 @@
 #include <string.h>
 
 int main() {
-    char names[100][50];  // array to store names
+    char names[100][50];  
     int n, i, j;
 
     printf("Enter number of names: ");
     scanf("%d", &n);
-    getchar(); // remove the leftover newline
+    getchar();
 
     printf("Enter %d names:\n", n);
     for (i = 0; i < n; i++) {
         fgets(names[i], sizeof(names[i]), stdin);
-        int len = strlen(names[i]);        // find the length of the string
+        int len = strlen(names[i]);        
 	if (names[i][len - 1] == '\n') {
 		names[i][len - 1] = '\0';}
     }
 
     printf("\nDuplicate names are:\n");
     for (i = 0; i < n; i++) {
-        char *p1 = names[i];  // pointer to first name
+        char *p1 = names[i];
         for (j = i + 1; j < n; j++) {
-            char *p2 = names[j];  // pointer to second name
+            char *p2 = names[j]; 
 
-            if (strcmp(p1, p2) == 0) {  // compare using pointer variables
+            if (strcmp(p1, p2) == 0) { 
                 printf("%s\n", p1);
-                break; // avoid printing the same name multiple times
+                break; 
             }
         }
     }
