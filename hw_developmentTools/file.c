5b798e50fd8250d77a32133b76898d4124d42483
Meri Hovsepyan <mhovsepyan001@gmail.com>
2025-12-07 15:37:36 +0400

    Add bubble sort implementation in file.c
    Implement bubble sort algorithm to sort an array.

--- hw_developmentTools/file.c
+++ hw_developmentTools/file.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
 
+#include <stdlib.h>
+
+
+void sortArray(int *arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+
+    sortArray(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    //free(arr);
+    return 0;
+}
