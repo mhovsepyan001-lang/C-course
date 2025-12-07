7b359f66b18845c80cd6a3871ef73c301e827d39
Meri Hovsepyan <mhovsepyan001@gmail.com>
2025-12-07 15:15:09 +0400

    Refactor line reading logic for clarity

--- hw_pointers_advanced/pointers_advanced3.c
+++ hw_pointers_advanced/pointers_advanced3.c
@@ -6,26 +6,21 @@
 #define MAX_LEN 1000   
 
 int main(int argc, char *argv[]) {
-    int n = 10;  // default number of lines
-
-    // if the user wrote something like "-5"
+    int n = 10; 
     if (argc == 2 && argv[1][0] == '-') {
-        n = atoi(argv[1] + 1);  // convert "5" to integer 5
+        n = atoi(argv[1] + 1);
     }
 
     char lines[MAX_LINES][MAX_LEN];
     int count = 0; 
 
-    // Read lines until end of input
     while (fgets(lines[count], MAX_LEN, stdin) != NULL) {
         count++;
-        if (count >= MAX_LINES) break; // stop if too many lines
+        if (count >= MAX_LINES) break; 
     }
 
-    // If user asked more lines than exist
     if (n > count) n = count;
 
-    // Print only last n lines
     for (int i = count - n; i < count; i++) {
         printf("%s", lines[i]);
     }
