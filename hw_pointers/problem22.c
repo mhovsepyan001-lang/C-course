70b6b4db9dd53fda52141b636fd5f144b88a8a48
Meri Hovsepyan <mhovsepyan001@gmail.com>
2025-12-07 15:12:29 +0400

    Update problem22.c

--- hw_pointers/problem22.c
+++ hw_pointers/problem22.c
@@ -9,21 +9,19 @@ int main() {
     printf("Enter a string: ");
     fgets(arr, sizeof(arr), stdin);
 
-    // Remove newline character if present
     length = strlen(arr);
     if (arr[length - 1] == '\n') {
         arr[length - 1] = '\0';
         length--;
     }
 
-    // Set pointer to point to the last character of the string
     pointer = arr + length - 1;
 
     printf("Reversed string: ");
-    // Print characters in reverse using the pointer
+
     while (pointer >= arr) {
-        printf("%c", *pointer); // Print the character pointed by pointer
-        pointer--;              // Move pointer backward
+        printf("%c", *pointer); 
+        pointer--;        
     }
     printf("\n");
 
